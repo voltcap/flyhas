4e11dc33d858ee02115f08c8f8fef96d9bfd923a
voltcap <121193901+voltcap@users.noreply.github.com>
2025-04-09 19:06:52 +0300

    Update and rename seats.java to Seats.java

---
+++ src/main/java/com/example/flyhas/model/Seats.java
@@ -0,0 +1,46 @@
+package com.example.flyhas.model;
+
+import jakarta.persistence.*;
+
+@Entity
+public class Seat {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String seatNumber;  
+    private boolean booked;
+
+    @ManyToOne
+    @JoinColumn(name = "flight_id")
+    private Flight flight;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSeatNumber() {
+        return seatNumber;
+    }
+
+    public void setSeatNumber(String seatNumber) {
+        this.seatNumber = seatNumber;
+    }
+
+    public boolean isBooked() {
+        return booked;
+    }
+
+    public void setBooked(boolean booked) {
+        this.booked = booked;
+    }
+
+    public Flight getFlight() {
+        return flight;
+    }
+
+    public void setFlight(Flight flight) {
+        this.flight = flight;
+    }
+}
