7c49cfb0738cdaed0fab3c2795b2b469eac6eb84
voltcap <121193901+voltcap@users.noreply.github.com>
2025-04-09 19:08:05 +0300

    Create SeatRepository.java

---
+++ src/main/java/com/example/flyhas/repository/SeatRepository.java
@@ -0,0 +1,12 @@
+package com.example.flyhas.repository;
+
+import com.example.flyhas.model.Seat;
+import com.example.flyhas.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SeatRepository extends JpaRepository<Seat, Long> {
+    List<Seat> findByFlight(Flight flight);
+    Seat findBySeatNumberAndFlight(String seatNumber, Flight flight);
+}
